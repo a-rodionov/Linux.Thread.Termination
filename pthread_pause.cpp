b6eccb64aa37287f0db418c57b717b2c3d7a341d
Andrew Rodionov <camelot58@gmail.com>
2018-12-06 11:31:35 -0800

    - added example with muting/unmuting suspend thread signal.

--- pthread_pause.cpp
+++ pthread_pause.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <thread>
@@ -18,10 +19,15 @@ bool ready = false;
 const static int sleep_period_sec = 1;
 std::atomic_bool wasPaused( false );
 std::atomic_bool anyError( false );
+std::atomic_bool isStopPhase1( false );
+std::atomic_bool isStopPhase2( false );
 
-void SuspendHandler( int /*signal*/ )
+
+void SuspendHandler(int /*signal*/)
 {
-  int pendingSignal = 0;  
+  std::cout << "SuspendHandler\n";
+
+  int pendingSignal = 0;
   sigset_t set;
 
   if(sigemptyset( &set ))
@@ -29,13 +35,13 @@ void SuspendHandler( int /*signal*/ )
     anyError = true;
     return;
   }
-  if(sigaddset( &set, RESUME_SIGNAL ))
+  if(sigaddset(&set, RESUME_SIGNAL))
   {
     anyError = true;
     return;
   }
 
-  if(sigwait( &set, &pendingSignal ))
+  if(sigwait(&set, &pendingSignal))
   {
     anyError = true;
     return;
@@ -50,28 +56,75 @@ void SuspendHandler( int /*signal*/ )
   wasPaused = true;
 }
 
-bool SetSignalHandler()
+bool MakeThreadSuspendable()
 {
+  sigset_t mask;
+
+  if(sigemptyset(&mask))
+  {
+   return false;
+  }
+  if(sigaddset(&mask, SUSPEND_SIGNAL))
+  {
+    return false;
+  }
+  if(sigaddset(&mask, RESUME_SIGNAL))
+  {
+    return false;
+  }
+  auto error = pthread_sigmask(SIG_UNBLOCK, &mask, nullptr);
+  if(error)
+  {
+   return false;
+  }
+
   struct sigaction act;
-  memset( &act, 0, sizeof( act ));
+  memset(&act, 0, sizeof(act));
 
   sigset_t set;
-  if(sigemptyset( &set ))
-  {
-    return false;
-  }
-  if(sigaddset( &set, RESUME_SIGNAL ))
-  {
-    return false;
-  }
-  if(sigaddset( &set, SUSPEND_SIGNAL ))
+  if(sigfillset(&set))
   {
     return false;
   }
   act.sa_mask = set;
 
   act.sa_handler = SuspendHandler;
-  if(sigaction( SUSPEND_SIGNAL, &act, 0 ))
+  if(sigaction(SUSPEND_SIGNAL, &act, 0))
+  {
+    return false;
+  }
+
+  act.sa_handler = SIG_IGN;
+  if(sigaction(RESUME_SIGNAL, &act, 0))
+  {
+    return false;
+  }
+  return true;
+}
+
+bool MuteSuspendSignal(struct sigaction* oldact)
+{
+  struct sigaction act;
+  memset( &act, 0, sizeof( act ));
+
+  sigset_t set;
+  if( sigfillset( &set ) )
+  {
+    return false;
+  }
+  act.sa_mask = set;
+
+  act.sa_handler = SIG_IGN;
+  if(sigaction( SUSPEND_SIGNAL, &act, oldact ))
+  {
+    return false;
+  }
+  return true;
+}
+
+bool UnmuteSuspendSignal(struct sigaction* oldact)
+{
+  if(sigaction(SUSPEND_SIGNAL, oldact, nullptr))
   {
     return false;
   }
@@ -80,18 +133,37 @@ bool SetSignalHandler()
 
 void ThreadFunction()
 {
+  struct sigaction oldact;
+  if(!MuteSuspendSignal( &oldact))
+  {
+    std::cout << "Failed to mute suspend signal\n";
+  }
+  std::cout << "Thread muted suspend signal\n";
+
   ready = true;
   cv.notify_one();
 
-  while(!wasPaused || anyError)
+  while(!anyError && !isStopPhase1)
   {
-    sleep(sleep_period_sec);
+    std::cout << '.';
+  }
+
+  if(!UnmuteSuspendSignal(&oldact))
+  {
+    std::cout << "Failed to unmute suspend signal\n";
+  }
+  std::cout << "Thread unmuted suspend signal\n";
+
+  while(!anyError && !isStopPhase2)
+  {
+    std::cout << '.';
   }
 }
 
+
 int main()
 {
-  if(!SetSignalHandler())
+  if(!MakeThreadSuspendable())
   {
     std::cout << "Failed to set signal handler." << std::endl;
     return 0;
@@ -103,17 +175,57 @@ int main()
     cv.wait(lk, [] { return ready; });
   }
 
-  if(pthread_kill(thread.native_handle(), SUSPEND_SIGNAL))
-  {
-    std::cout << "Failed to send SUSPEND_SIGNAL." << std::endl;
-    return 0;
-  }
-  sleep(sleep_period_sec);
+  usleep(50);
+  std::cout << "main sends resume signal\n";
+
   if(pthread_kill(thread.native_handle(), RESUME_SIGNAL))
   {
     std::cout << "Failed to send RESUME_SIGNAL." << std::endl;
     return 0;
   }
+
+  usleep(50);
+  std::cout << "main sends suspend signal\n";
+
+  if(pthread_kill(thread.native_handle(), SUSPEND_SIGNAL))
+  {
+    std::cout << "Failed to send SUSPEND_SIGNAL." << std::endl;
+    return 0;
+  }
+
+  usleep(50);
+  std::cout << "main sends resume signal\n";
+
+  if(pthread_kill(thread.native_handle(), RESUME_SIGNAL))
+  {
+    std::cout << "Failed to send RESUME_SIGNAL." << std::endl;
+    return 0;
+  }
+
+  std::cout << "main sets stopPhase1 flag\n";
+  isStopPhase1 = true;
+
+  usleep(50);
+  std::cout << "main sends suspend signal\n";
+
+  if(pthread_kill(thread.native_handle(), SUSPEND_SIGNAL))
+  {
+    std::cout << "Failed to send SUSPEND_SIGNAL." << std::endl;
+    return 0;
+  }
+
+  usleep(50);
+  std::cout << "main sends resume signal\n";
+
+  if(pthread_kill(thread.native_handle(), RESUME_SIGNAL))
+  {
+    std::cout << "Failed to send RESUME_SIGNAL." << std::endl;
+    return 0;
+  }
+
+  usleep(50);
+  std::cout << "main sets stopPhase2 flag\n";
+  isStopPhase2 = true;
 
   if(!thread.joinable())
   {
